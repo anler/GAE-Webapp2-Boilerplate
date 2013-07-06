f8d85620acac5591aba112fe85b43691baf821b8
ikame <anler86@gmail.com>
2013-07-06 22:36:13 +0200

    Handling requests with 'view' function

--- README.md
+++ README.md
@@ -3,3 +3,36 @@ Google App Engine application boilerplate
 
 Boilerplate application for Google App Engine using the Webapp2 web
 framework and Jinja2 templating system.
+
+Demo
+----
+
+```python
+from boiler import application
+
+
+class IndexPage(application.TemplateRequestHandler):
+    def get(self):
+        self.render_to_response("index.html")
+
+
+def say_hello(handler):
+    return 'Hello world!'
+
+
+def say_hi(handler):
+    handler.response.write('Hi world!')
+
+
+def index_page(handler):
+    handler.render_to_response("index.html")
+
+
+app = application.WSGIApplication([
+    ('/say-hello', application.view(say_hello)),
+    ('/say-hi', application.view(say_hi)),
+    ('/index.html', application.view(
+        index_page, handler_class=application.TemplateRequestHandler)),
+    ('/', IndexPage)
+])
+```
--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from boiler.application import TemplateRequestHandler, view, WSGIApplication
+from boiler import application
 
 
-class IndexPage(TemplateRequestHandler):
+class IndexPage(application.TemplateRequestHandler):
     def get(self):
         self.render_to_response("index.html")
 
 
 def say_hello(handler):
-    return 'Hello world! <a href="index.html">Visit main page</a>'
+    return 'Hello world!'
 
 
-app = WSGIApplication([
-    ('/', view(say_hello)),
-    ('/index.html', IndexPage)
+def say_hi(handler):
+    handler.response.write('Hi world!')
+
+
+def index_page(handler):
+    handler.render_to_response("index.html")
+
+
+app = application.WSGIApplication([
+    ('/say-hello', application.view(say_hello)),
+    ('/say-hi', application.view(say_hi)),
+    ('/index.html', application.view(
+        index_page, handler_class=application.TemplateRequestHandler)),
+    ('/', IndexPage)
 ])
